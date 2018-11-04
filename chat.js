bfed496d1987eda13356de262f42e144d2193b81
Kevin Giovanni <kevin.giovanni@ti.ukdw.ac.id>
2018-11-04 22:06:07 +0700

    send message private and group

--- chat.js
+++ chat.js
@@ -50,12 +50,16 @@ app.get('/assets/css/index.css', function(req, res) {
     res.sendFile(__dirname + '/assets/css/index.css');
 });
 
+app.get('/assets/css/select2.min.css', function(req, res) {
+    res.sendFile(__dirname + '/assets/css/select2.min.css');
+});
+
 app.get('/assets/js/login.js', function(req, res) {
     res.sendFile(__dirname + '/assets/js/login.js');
 });
 
-app.get('/assets/js/custom-login.js', function(req, res) {
-    res.sendFile(__dirname + '/assets/js/custom-login.js');
+app.get('/assets/js/select2.min.js', function(req, res) {
+    res.sendFile(__dirname + '/assets/js/select2.min.js');
 });
 
 // routing
@@ -88,6 +92,8 @@ app.post('/regisAuth', function(req, res){
     console.log('user registered');
 });
 
+var logged_in = null;
+
 app.post('/loginAuth', function (req, res) {
     var sql = "select name, username, id from users where username = ? and password = ?";
     var username = req.body.lg_username;
@@ -103,7 +109,9 @@ app.post('/loginAuth', function (req, res) {
             connection.query(sql, [req.sessionID,req.session.user.username], function (err, result) {
                 if (err) throw err;
                 if (result.affectedRows == 1) {
-                    return res.redirect('/chat');
+                    res.send({ user_id: req.session.user.id});
+                    logged_in = req.session.user.id;
+                    // return res.redirect('/chat');
                 }
             });
             console.log(username + ' logged in!');
@@ -117,13 +125,12 @@ app.get('/getLoggedInUser', function (req, res) {
 });
 
 app.get('/getChatMessage', function (req, res) {
-    var sql = "select * from messages where room_id = ?";
+    var sql = "select * from messages m, users u where m.user_id = u.id and room_id = ?";
     connection.query(sql, req.query.room_id, function (err, results) {
         if (err) throw err;
         res.send(results);
     });
 });
-
 app.get('/getOnlineUsers', function (req, res) {
     var sql = "select name, username, id from users where payload is not null and id != ?";
     connection.query(sql, req.session.user.id, function (err, results) {
@@ -144,21 +151,25 @@ app.get('/getOnlineUsers', function (req, res) {
 });
 
 app.post('/newPrivateChat', function(req, res){
-    var target_user = req.body.user_id;
-    var string1 = req.session.user.username + '-' + req.target_user.username;
-    var string2 = req.target_user.username + '-' + req.session.user.username;
+    var target_user = req.body.user;
+    var string1 = req.session.user.name + target_user.name;
+    var string2 = target_user.name + req.session.user.name;
     var sql = "select distinct rd.room_id, r.name from rooms r, room_details rd where r.id = rd.room_id and r.name in (?,?) and r.type = 'private'";
     connection.query(sql, [string1, string2], function (err, result) {
         if(result.length == 1) {
             res.send(result)
         }else {
-            var sql = "insert into rooms (username, password, name) values (?,?,?)";
-            connection.query(sql, [username, password, name],function (err, result) {
+            var sql = "insert into rooms (name, type) values (?,'private')";
+            connection.query(sql, string1,function (err, result) {
                 if (err) throw err
-                var sql = "select distinct rd.room_id, r.name from rooms r, room_details rd where r.id = rd.room_id and r.name in (?,?) and r.type = 'private'";
-                connection.query(sql, [string1, string2], function (err, result) {
-                    if (err) throw err;
-
+                var sql = "insert into room_details (room_id, user_id) values (?,?), (?,?)";
+                connection.query(sql, [result.insertId, req.session.user.id, result.insertId, target_user.id],function (err, result) {
+                    if (err) throw err
+                    var sql = "select distinct rd.room_id, r.name from rooms r, room_details rd where r.id = rd.room_id and r.name in (?,?) and r.type = 'private'";
+                    connection.query(sql, [string1, string2], function (err, result) {
+                        if (err) throw err;
+                        res.send(result);
+                    });
                 });
             });
         }
@@ -180,12 +191,24 @@ app.get('/logout', function (req, res) {
 });
 
 app.get('/getRecentRoomChats', function (req, res) {
-    var sql = "select u.id, r.name as room_name, r.type, u.name, u.username, rd.room_id from room_details rd, rooms r, users u where u.id = rd.user_id and r.id = rd.room_id and u.id != ? order by r.timestamp";
+    var sql = "SELECT rooms.id as room_id, users.name, rooms.name as room_name, rooms.type, users.id, users.username FROM rooms INNER JOIN room_details ON room_details.room_id = rooms.id INNER JOIN users ON users.id = room_details.user_id WHERE room_details.user_id = ?";
     connection.query(sql, req.session.user.id, function (err, results) {
         if (err) throw err;
         var data = {
             total: results.length,
             chats: results
+        }
+        res.send(data);
+    });
+});
+
+app.get('/getAvailableUsers', function (req,res) {
+    var sql = "select id, name from users where id not in (select user_id from room_details where room_id = ?)";
+    connection.query(sql, req.query.room_id, function (err, results) {
+        if (err) throw err;
+        var data = {
+            total: results.length,
+            users: results
         }
         res.send(data);
     });
@@ -201,26 +224,80 @@ const self = this;
 io.on('connect', (socket) => {
     console.log('connected!');
 
-    socket.on('room', (room) => {
-        socket.join(room);
-        // var messages = getMessages(room);
-        io.sockets.in("abc123").emit('message', 'what is going on, party people?');
-        io.sockets.in('foobar').emit('message', 'anyone in this room yet?');
+    socket.on('online_users', () => {
+        var sql = "select name, username, id from users where payload is not null and id != ?";
+        connection.query(sql, logged_in, function (err, results) {
+            if (err) throw err;
+            if (results.length > 0) {
+                var userOnline = {
+                    total: results.length,
+                    users: results
+                };
+            }else {
+                var userOnline = {
+                    total: 0,
+                    data: []
+                };
+            }
+            io.sockets.emit('online_users', userOnline);
+        });
     });
 
-    socket.on('get_recent_chat', (username) => {
-        io.sockets.emit('recent_chat', getRecentChats(username));
+    socket.on('SEND_MESSAGE', (data) => {
+        var sql = "insert into messages (message, user_id, room_id) values (?,?,?)";
+        connection.query(sql, [data.message, data.user.id, data.room_id],function (err, result) {
+            if (err) throw err
+        });
+        socket.join(data.room_id);
+        var message = {
+            message: data.message,
+            user_id: data.user.id,
+            room_id: data.room_id
+        };
+        io.sockets.in(data.room_id).emit('GET_MESSAGE', message);
     });
+
+    socket.on('INVITE_USER', (data) => {
+        // console.log(data);
+        var temp_id = [];
+        var sql = "insert into room_details (room_id, user_id) values ";
+        var count = data.user_id.length;
+        var index = 1;
+        data.user_id.forEach((item) => {
+            if(index == count) {
+                sql += '(?,?)';
+            } else {
+                sql += '(?,?),';
+            }
+            console.log(item);
+            temp_id.push(data.room_id);
+            temp_id.push(item);
+            index++;
+        });
+
+        connection.query(sql, temp_id,function (err, result) {
+            if (err) throw err
+        });
+
+        io.sockets.emit('INVITED', {change: true});
+    })
 });
 
-function getMessages(room) {
-    var sql = "select * from messages m, rooms r, users u where u.id = m.user_id and m.room_id = r.id and r.name = ?";
-    connection.query(sql, room, function (err, results) {
+function getOnlineUsers(user_id) {
+    var sql = "select name, username, id from users where payload is not null and id != ?";
+    connection.query(sql, user_id, function (err, results) {
         if (err) throw err;
-        var data = [];
         if (results.length > 0) {
-            data = results
+            var userOnline = {
+                total: results.length,
+                users: results
+            };
+        }else {
+            var userOnline = {
+                total: 0,
+                data: []
+            };
         }
-        return data;
+        return userOnline;
     });
 }
